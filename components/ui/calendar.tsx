5be5f728735c5cd6d0a64f11a3f7276073a69a77
PriyanshuBh <priyanshubh2003@gmail.com>
2025-06-05 17:35:31 +0530

    update

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { ChevronLeft, ChevronRight } from "lucide-react"
+import { ChevronLeftIcon, ChevronRightIcon } from '@radix-ui/react-icons';
 import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
@@ -54,13 +54,12 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-        ),
+        Chevron: ({ orientation }) => {
+          const Icon = orientation === 'left' ? ChevronLeftIcon : ChevronRightIcon;
+          return <Icon className='h-4 w-4' />;
+        },
       }}
+      
       {...props}
     />
   )
